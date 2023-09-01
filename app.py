cf3228986a9bae542910163e3fb827d2f432d17a
saiphanikumar235 <138872112+saiphanikumar235@users.noreply.github.com>
2023-09-01 19:12:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import fitz  # PyMuPDF library for PDF processing
+import aspose.words as aw
+import docx2txt
 
 st.title("Resume Parser with OpenAI GPT-3")
 
@@ -11,22 +12,8 @@ if uploaded_resume is not None:
 
     # Check if the uploaded file is a PDF
     if uploaded_resume.type == "application/pdf":
-        # Extract text from the PDF using PyMuPDF (Fitz)
-        pdf_doc = fitz.open(stream=uploaded_resume.read(), filetype="pdf")
-        for page_num in range(pdf_doc.page_count):
-            page = pdf_doc.load_page(page_num)
-            resume_text += page.get_text()
-
-    else:
-        # If it's not a PDF, assume it's plain text
-        resume_text = uploaded_resume.read()
-
-    # Display the extracted text
-    st.subheader("Extracted Resume Text")
-    st.write(resume_text)
-
-    if st.button("Parse Resume"):
-        # Parse the extracted text with OpenAI GPT-3
-        parsed_info = parse_resume(resume_text)  # You can reuse the parse_resume function from the previous code
-        st.subheader("Parsed Information")
-        st.write(parsed_info)
+        doc = aw.Document(uploaded_resume)
+        doc.save("Output1.docx")
+        my_text = docx2txt.process("./Output1.docx")
+        st.write(my_text)
+        
